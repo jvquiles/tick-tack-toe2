caee7192f89d3bf2ccaf8604e21e750134cd8ce2
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-13 11:49:32 +0200

    chore: Create projects

---
+++ TickTackToe2/TickTackToe2.Tests/GameShould.cs
@@ -0,0 +1,16 @@
+namespace TickTackToe2.Tests
+{
+    public class GameShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
