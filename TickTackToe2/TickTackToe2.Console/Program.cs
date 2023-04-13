03f0e342d11b51e5c3580ca3023a62787b31bbfe
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-13 12:53:04 +0200

    feat: Implement play more than once

--- TickTackToe2/TickTackToe2.Console/Program.cs
+++ TickTackToe2/TickTackToe2.Console/Program.cs
@@ -1,13 +1,16 @@
 ﻿using TickTackToe2.Console;
 
 var game = new Game();
-Console.WriteLine(game.PrintBoard());
 
-Console.Write("X: ");
-var coordinateX = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y: ");
-var coordinateY = Convert.ToInt32(Console.ReadLine());
+do
+{
+    Console.Clear();
+    Console.WriteLine(game.PrintBoard());
 
-game.Play(coordinateX, coordinateY);
-Console.Clear();
-Console.WriteLine(game.PrintBoard());
+    Console.Write("Enter coordinates for X: ");
+    var coordinateX = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter coordinates for Y: ");
+    var coordinateY = Convert.ToInt32(Console.ReadLine());
+
+    game.Play(coordinateX, coordinateY);
+} while (true);
