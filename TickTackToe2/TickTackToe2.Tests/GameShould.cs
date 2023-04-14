26bf68cd1cdf3a6886b799bd3b57e838de1c84d4
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-14 15:01:24 +0200

    test: Add player X wins by completing the third row

--- TickTackToe2/TickTackToe2.Tests/GameShould.cs
+++ TickTackToe2/TickTackToe2.Tests/GameShould.cs
@@ -96,5 +96,20 @@ namespace TickTackToe2.Tests
 
             status.Should().Be("Player X wins");
         }
+
+        [Test]
+        public void LetWinPlayerXByThirdRow()
+        {
+            var game = new Game();
+            game.Play(new Coordinates(2, 0));
+            game.Play(new Coordinates(0, 0));
+            game.Play(new Coordinates(2, 1));
+            game.Play(new Coordinates(0, 1));
+            game.Play(new Coordinates(2, 2));
+
+            var status = game.GetStatus();
+
+            status.Should().Be("Player X wins");
+        }
     }
 }
