3f9202210fa0c930d34318c3cecf9d62ead1b049
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-14 15:09:04 +0200

    feat: player X wins by completing the third row

--- TickTackToe2/TickTackToe2.Console/Game.cs
+++ TickTackToe2/TickTackToe2.Console/Game.cs
@@ -56,6 +56,11 @@ public class Game
             return "Player X wins";
         }
 
+        if (_board[2, 0] == Token.X && _board[2, 1] == Token.X && _board[2, 2] == Token.X)
+        {
+            return "Player X wins";
+        }
+
         return string.Empty;
     }
 }
