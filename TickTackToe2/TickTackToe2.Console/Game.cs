6e2316e61314c5b58cd5513659be6518b17df76c
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-18 10:44:03 +0200

    refact: Reuse row completion logic

--- TickTackToe2/TickTackToe2.Console/Game.cs
+++ TickTackToe2/TickTackToe2.Console/Game.cs
@@ -46,17 +46,7 @@ public class Game
 
     public string GetStatus()
     {
-        if (IsFirstRowCompletedByX())
-        {
-            return "Player X wins";
-        }
-
-        if (IsSecondRowCompletedByX())
-        {
-            return "Player X wins";
-        }
-
-        if (IsThirdRowCompletedByX())
+        if (IsRowCompletedByX(0) || IsRowCompletedByX(1) || IsRowCompletedByX(2))
         {
             return "Player X wins";
         }
@@ -64,18 +54,8 @@ public class Game
         return string.Empty;
     }
 
-    private bool IsThirdRowCompletedByX()
+    private bool IsRowCompletedByX(int row)
     {
-        return _board[2, 0] == Token.X && _board[2, 1] == Token.X && _board[2, 2] == Token.X;
-    }
-
-    private bool IsSecondRowCompletedByX()
-    {
-        return _board[1, 0] == Token.X && _board[1, 1] == Token.X && _board[1, 2] == Token.X;
-    }
-
-    private bool IsFirstRowCompletedByX()
-    {
-        return _board[0, 0] == Token.X && _board[0, 1] == Token.X && _board[0, 2] == Token.X;
+        return _board[row, 0] == Token.X && _board[row, 1] == Token.X && _board[row, 2] == Token.X;
     }
 }
