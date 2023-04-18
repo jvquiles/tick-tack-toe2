79ce3ba952cc359b2d1b58e47c8cb55e353de142
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-18 14:07:20 +0200

    feat: Implement finishing the game

--- TickTackToe2/TickTackToe2.Console/Program.cs
+++ TickTackToe2/TickTackToe2.Console/Program.cs
@@ -12,5 +12,17 @@ do
     Console.Write("Enter coordinates for Y: ");
     var coordinateY = Convert.ToInt32(Console.ReadLine());
 
-    game.Play(new Coordinates(coordinateX, coordinateY));
-} while (true);
+    try
+    {
+        game.Play(new Coordinates(coordinateX, coordinateY));
+    }
+    catch (Exception _)
+    {
+        Console.WriteLine("The move cannot be performed due to an error. Try again.");
+        Console.ReadLine();
+    }
+} while (game.GetStatus() == string.Empty);
+
+
+Console.Clear();
+Console.WriteLine(game.PrintBoard());
